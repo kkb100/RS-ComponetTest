1d0c1eab6974955a050f6b4fb5fd18ec062cc93a
Kelvin Boakye <kkboakye45@yahoo.co.uk>
2019-03-18 16:49:42 +0000

    code refator

--- my-app/src/test/java/com/RSComponet/app/PageObject/BasketPage.java
+++ my-app/src/test/java/com/RSComponet/app/PageObject/BasketPage.java
@@ -2,15 +2,10 @@ package com.RSComponet.app.PageObject;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
 
 public class BasketPage {
 
     protected final WebDriver driver;
-
-    @FindBy(id="checkoutSecurelyAndPuchBtn")
-    WebElement CheckoutPurchaseBtn = null;
 
 
     public BasketPage(WebDriver driver) {
